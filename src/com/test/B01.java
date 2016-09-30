b8dc8b0f2db9bc63b2293c0c732f54ceb11ccc83
leeym008 <leeym008@naver.com>
2016-09-30 19:31:07 +0900

    파일 수정

---
+++ src/com/test/B01.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class B01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
