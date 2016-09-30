18ac4f2753d38a66cfc824090c924fef28496545
leeym008 <leeym008@naver.com>
2016-09-30 20:12:20 +0900

    6주차 과제 1

--- src/com/test/B01.java
+++ src/com/test/B01.java
@@ -1,15 +1,29 @@
 package com.test;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class B01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("FixB01");
+		int max_num, min_num;
+		
+		int [] num = new int[3];
+		
+		for (int i=0; i<3; i++){
+		System.out.printf("%d번째 숫자를 입력하세요 : ",i+1);
+		num[i]=s.nextInt();
+		}
+		
+		max_num=num[0];
+		min_num=num[0];
+		
+		for (int i=0; i<3; i++){
+			if (max_num > num[i])
+				min_num=num[i];
+			if (min_num < num[i])
+				max_num=num[i];
+		}
+		System.out.printf("최댓값 : %d 최솟값 : %d",max_num,min_num);
 	}
-
 }
